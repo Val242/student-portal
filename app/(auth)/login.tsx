b8589f91f050de157d73f362a5dcc50b2d55d86a
=Val242 <=ebongvalentine70@gmail.com>
2026-04-09 09:22:27 +0100

    Did work on the auth ui

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,18 +1,81 @@
 import { createAuthStyles } from '@/assets/styles/auth.styles'
 import { ROUTES } from '@/constants/navigation'
+import { Ionicons } from '@expo/vector-icons'
 import { router } from 'expo-router'
 import React from 'react'
-import { Text, TouchableOpacity } from 'react-native'
+import { Pressable, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const login = () => {
-    const authStyles = createAuthStyles()
+      const [user, setUser] = React.useState({
+        email: 'ebongvalentine70@gmail.com',
+        password: '12345678',
+      });
+
+      const handleChange = (field: string, value: string) => {
+      setUser((prev) => ({
+        ...prev,
+        [field]: value,
+      }));
+};
+      const authStyles = createAuthStyles()
+      
+      const OnPressFunction = ()=>{
+        alert("Hello")
+      }
+
   return (
-    <SafeAreaView style= {authStyles.container}>
-      <Text style={authStyles.header}>login Valentine</Text>
-                <TouchableOpacity onPress={() => router.replace(ROUTES.SIGNUP)}>
-            <Text >Sign Up</Text>
-          </TouchableOpacity>
+    
+    <SafeAreaView  style={authStyles.container}>
+
+        <Text style={authStyles.title}>Welcome Back</Text>
+  
+
+      <TextInput
+        value={user.email}
+        onChangeText={(text) => handleChange('email', text)}
+        style={authStyles.input}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+
+
+      <TextInput
+        value={user.password}
+        onChangeText={(text) => handleChange('password', text)}
+        style={authStyles.input}
+        // secureTextEntry
+      />
+
+
+      <Pressable onPress={OnPressFunction} style={authStyles.cta}>
+            <Text style={{color: 'white', textAlign: 'center', fontWeight: "700"}}>Login </Text>
+      </Pressable>
+
+      <Text> or </Text>
+
+      <View style={{display:'flex', flexDirection: 'row', justifyContent:"space-around", marginVertical: 15 }}>
+            <View style={{display:'flex', flexDirection: 'row', }}>
+              <Ionicons name= "logo-apple" size={30} color='gray'/>
+              <Text style={{fontSize:20, color:'gray'}}> Apple </Text>
+            </View>
+
+            <View style={{display:'flex', flexDirection: 'row', }}>
+              <Ionicons name= "logo-google" size={30} />
+              <Text style={{fontSize:20, color:'gray'}}> Google </Text>
+            </View>
+      </View>
+
+      <View style={{marginVertical:15}}>
+        <Text style={{textAlign: 'center'}}>
+          Already have an account?
+           <TouchableOpacity onPress={() => router.replace(ROUTES.SIGNUP)}>
+           <Text style= {{color:'#a28ef9', fontWeight:'bold'}}>
+             Sign Up</Text>
+             </TouchableOpacity>
+             </Text>
+      </View>
+
     </SafeAreaView>
   )
 }
