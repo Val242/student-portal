e0c033c9e9b1224482900d3a354c9eb947c8731d
=Val242 <=ebongvalentine70@gmail.com>
2026-04-16 07:40:00 +0100

    Improvement

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,29 @@
+import { ROUTES } from "@/constants/navigation";
 import { AuthProvider, useAuth } from "@/context/AuthContext";
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import { useEffect } from "react";
 
 function RootNavigator() {
   const { authState } = useAuth();
+  console.log("Auth state in navigator:", authState)
+
+    const router = useRouter();
+
+  useEffect(() => {
+    if (authState?.authenticated) {
+      router.replace(ROUTES.WIDGETS);
+    } else if (authState?.authenticated === false) {
+      router.replace(ROUTES.LOGIN);
+    }
+  }, [authState?.authenticated]);
 
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      {authState?.authenticated ? (
-        // ✅ Protected routes
-        <Stack.Screen name="(tabs)" />
-      ) : (
-        // ❌ Public routes
-        <Stack.Screen name="(auth)" />
-      )}
+        {authState?.authenticated ? (
+    <Stack.Screen name="(tabs)" />
+  ) : (
+    <Stack.Screen name="(auth)" />
+  )}
     </Stack>
   );
 }
